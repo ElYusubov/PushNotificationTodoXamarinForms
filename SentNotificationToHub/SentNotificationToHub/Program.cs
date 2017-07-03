f9ef777b274cee88bfcc9ec243110cd0e2fa856e
ElYusubov <elxany@hotmail.com>
2017-07-03 14:09:03 -0400

    Add project files.

---
+++ SentNotificationToHub/SentNotificationToHub/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Azure.NotificationHubs;
+
+namespace SentNotificationToHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendNotificationAsync();
+            Console.ReadLine();
+        }
+
+        private static async void SendNotificationAsync()
+        {
+            NotificationHubClient hub = NotificationHubClient.CreateClientFromConnectionString("Endpoint=sb://ns-notification-hub-17.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=7JgMPUZd4coxOv17V0gGQ5Pyd62adkc8Uw5Sa5Kc0XY=", "notification-hub-17");
+            await hub.SendGcmNativeNotificationAsync("{ \"data\" : {\"message\":\"Hello Evv Team!\"}}");
+        }
+    }
+}
---
+++ push_notification_todo/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace push_notification_todo
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://push-notification-todo.azurewebsites.net";
+
+	}
+}
+
